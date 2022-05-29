72003a5f2ddb8b3a84f22672d10b6d52e1a535ed
mettinger <jettinger35@gmail.com>
2022-05-29 17:16:25 -0400

    message

--- odeInfer/python/sindyTorch.py
+++ odeInfer/python/sindyTorch.py
@@ -2,6 +2,7 @@
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch.autograd import Variable
 import itertools
 
 # %%
@@ -54,13 +55,14 @@ def gridGet(bounds, steps):
     return list(itertools.product(*x))
 
 class epileptorDataset(Dataset):
-    def __init__(self, odeSystem, bounds, step):
+    def __init__(self, odeSystem, bounds, step, basisFunctions):
 
         systemDim = len(step)
-        x = gridGet(bounds, step)
-        xDot = [np.array(odeSystem(*i)).reshape((1, systemDim)) for i in x]
+        numBasis = len(basisFunctions)
+        grid = gridGet(bounds, step)
+        xDot = [np.array(odeSystem(*i)).reshape((1, systemDim)) for i in grid]
 
-        self.x = torch.Tensor([np.array(i).reshape((1,systemDim)) for i in x])
+        self.x = torch.Tensor([np.array([f(i) for f in basisFunctions]).reshape((1, numBasis)) for i in grid])
         self.xDot = torch.Tensor(xDot)
 
     def __len__(self):
@@ -69,23 +71,89 @@ class epileptorDataset(Dataset):
     def __getitem__(self, idx):
         return self.x[idx,:], self.xDot[idx,:]
 
-#%%
+basisFunctions = [
+    lambda x : 1,
+    lambda x : x[0],
+    lambda x : x[1],
+    lambda x : x[2],
+    lambda x : x[0] ** 2,
+    lambda x : x[0] * x[1],
+    lambda x : x[0] * x[2],
+    lambda x : x[1] ** 2,
+    lambda x : x[1] * x[2],
+    lambda x : x[2] ** 2
+]
+
+def train_loop(dataloader, models, loss_fn, optimizers):
+    size = len(dataloader.dataset)
+
+    for batch, (X, y) in enumerate(dataloader):
+        for modelIndex, thisModel in enumerate(models):
+            thisModel.train()
+            optimizer = optimizers[modelIndex]
+
+            # Compute prediction and loss
+            pred = thisModel(Variable(X.cuda()))
+            loss = loss_fn(pred, Variable(y[:,:, modelIndex].cuda()))
+
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if batch % 100 == 0:
+                loss, current = loss.item(), batch * len(X)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def dataloaderGet(odeIndex, basisFunctions):
+
+    if odeIndex == 0:
+        odeSystem = epileptor
+        bounds = [(0,5) for i in range(6)]
+        steps = [6 for i in range(6)]
+        batchSize = 64
+    elif odeIndex == 1:
+        odeSystem = lorenz
+        bounds = [(-2,2), (-2,2), (-2,2)]
+        steps = [40, 40, 40]
+        batchSize = 64
+
+
+    dataset = epileptorDataset(odeSystem, bounds, steps, basisFunctions)
+    dataloader = DataLoader(dataset, batch_size=batchSize)
+
+    systemDim = len(steps)
+    return dataloader, systemDim
+
+class linearRegression(torch.nn.Module):
+    def __init__(self, inputSize, outputSize):
+        super(linearRegression, self).__init__()
+        self.linear = torch.nn.Linear(inputSize, outputSize, bias=False)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+# %%
 
 odeIndex = 1
+learningRate = .01
+epochs = 10
 
-if odeIndex == 0:
-    odeSystem = epileptor
-    bounds = [(0,5) for i in range(6)]
-    steps = [6 for i in range(6)]
-    batchSize = 64
-elif odeIndex == 1:
-    odeSystem = lorenz
-    bounds = [(-2,2), (-2,2), (-2,2)]
-    steps = [100, 100, 100]
-    batchSize = 64
+dataloader, systemDim = dataloaderGet(odeIndex, basisFunctions)
+numBasisFunctions = len(basisFunctions)
+models = [linearRegression(numBasisFunctions, 1).cuda() for i in range(systemDim)]
 
-dataset = epileptorDataset(odeSystem, bounds, steps)
-dataloader = DataLoader(dataset, batch_size=batchSize)
+# change loss function for l1!
+lossFunction = torch.nn.MSELoss() 
+optimizers = [torch.optim.SGD(thisModel.parameters(), lr=learningRate) for thisModel in models]
+
+#%%
+
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(dataloader, models, lossFunction, optimizers)
+print("Done!")
 
 
 # %%
